617e9b78d72b67dea1ac0e554cffa21d20611030
Jonatas Sampaio <aurijona192@gmail.com>
2026-05-02 01:41:55 -0300

    ClienteResponse agora retorna valor de status

--- src/main/java/br/com/unifan/backend_vendas_start/dtos/response/ClienteResponse.java
+++ src/main/java/br/com/unifan/backend_vendas_start/dtos/response/ClienteResponse.java
@@ -1,11 +1,14 @@
 package br.com.unifan.backend_vendas_start.dtos.response;
 
+import br.com.unifan.backend_vendas_start.entity.enums.Status;
+
 import java.util.UUID;
 
 public record ClienteResponse(
         UUID id,
         String nome,
         String email,
+        Status status,
         String cep
 
 ) {
